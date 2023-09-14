c9094973313a29f4d9cb4a779d709bb5425dcde0
internetrelative <45829953+internetrelative@users.noreply.github.com>
2023-09-14 08:35:33 +0800

    Update CC1101_Frequency_Finder.ino

--- CC1101_Frequency_Finder/CC1101_Frequency_Finder.ino
+++ CC1101_Frequency_Finder/CC1101_Frequency_Finder.ino
@@ -20,9 +20,24 @@ bool initcc1101() {
   }
 
   ELECHOUSE_cc1101.Init();
-  ELECHOUSE_cc1101.setRxBW(100.0);    // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
+  ELECHOUSE_cc1101.setRxBW(80.0);    // Set the Receive Bandwidth in kHz. Value from 58.03 to 812.50. Default is 812.50 kHz.
   ELECHOUSE_cc1101.setModulation(2);  // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
   return true;
+}
+
+int highestRssi = 0;           
+uint8_t highestRssiPosition = -1;  
+
+void getHighestRssi(int arr[], int size) {
+  highestRssi = arr[0];     
+  highestRssiPosition = 0; 
+
+  for (int i = 1; i < size; i++) {
+    if (arr[i] > highestRssi) {
+      highestRssi = arr[i];     
+      highestRssiPosition = i; 
+    }
+  }
 }
 
 void setup() {
@@ -34,27 +49,33 @@ void setup() {
     delay(1000);
   }
 }
-const int threshold = -75;  // Adjust threshold
-const uint8_t freqCount = 5;
-const float freq[freqCount] = { 310.0, 315.0, 318.0, 390.0, 433.92 };
+const int threshold = -85;  // Adjust threshold
+const uint8_t freqCount = 8;
+const float freq[freqCount] = { 310.0, 315.0, 318.0, 330.0, 390.0, 433.0, 433.92, 868.35 };
+int rssi[freqCount] = { 0 };
 
 void loop() {
   static uint8_t i = 0;
   uint32_t lastTime = millis();
-  int rssi = 0;
   ELECHOUSE_cc1101.setMHZ(freq[i]);
   ELECHOUSE_cc1101.SetRx();
   while (millis() - lastTime < 15) {  // listen for 15ms
     delay(5);
-    rssi = ELECHOUSE_cc1101.getRssi();
-    if (rssi > threshold) {
-      Serial.printf("%.2f MHz RSSI: %d\n", freq[i], rssi);
-      delay(400);
-      break;
-    }
+    rssi[i] = ELECHOUSE_cc1101.getRssi();
   }
 
   i++;
-  i = (i > freqCount - 1) ? 0 : i;
+  if (i > freqCount - 1) {
+    getHighestRssi(rssi, freqCount);
+    if (highestRssiPosition != -1 && highestRssi != 0) {
+      if (highestRssi > threshold) {
+        Serial.printf("%.2f MHz RSSI: %d\n", freq[highestRssiPosition], highestRssi);
+        delay(400);
+      }
+      highestRssi = 0;
+      highestRssiPosition = -1;
+    }
+    i = 0;
+  }
   ELECHOUSE_cc1101.setSidle();
 }
